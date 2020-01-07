0cffae830f7e20f38b211ab5622a6dc4afb2a10f
israelDuranV <30297088+israelDuranV@users.noreply.github.com>
2020-01-07 10:04:12 -0600

    Update autos.service.ts

--- src/app/services/autos.service.ts
+++ src/app/services/autos.service.ts
@@ -11,11 +11,11 @@ export class AutosService {
 
   constructor(private http: HttpClient) {}
   getAll(){
-    return this.http.get("http://34.231.146.47:3000/api/auth/pruebas@terantbwa.com.mx/Vpz6zPv:i!35LDs")
+    return this.http.get(YOUR_URL+":3000/api/auth/pruebasterantbwa.com.mx/Vpz6zPv:i!35LDs")
     .pipe(map( data => data));
   }
   getAutos(token: any){
-    return this.http.get(`http://34.231.146.47:3000/api/vehicles/?token=${token.token}`)
+    return this.http.get(YOUR_OTHER_URL+`:3000/api/vehicles/?token=${token.token}`)
     .pipe(map( data => data));
   }
 }
